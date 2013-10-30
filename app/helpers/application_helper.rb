d584eb965d47058232fad38cc720841839e71df8
Phuong Tran <>
2013-10-30 14:40:36 -0500

    Adding a view helper to template page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # Return the full title on a per-page basis
+  def full_title(page_title)
+    # Full title = base_title + possibly | page_title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    page_title.blank? ? base_title : "#{base_title} | #{page_title}"
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,27 +8,31 @@ describe "StaticPages" do
     end
     it "has the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App')
+    end
+    it "does not have a custom page title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title(' | Home')
     end
   end
   
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "has the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-    it "should have the title 'Help'" do
+    it "has the title 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
+    it "has the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
-    it "should have the title 'About Us'" do
+    it "has the title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
